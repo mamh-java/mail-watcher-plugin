ad3dc82e798a7115cae3abab4e4268d472cd1ff3
Mamh-windows <bright.ma@mage.com>
2020-08-16 15:51:14 +0800

    add execute scripts for windows or linux support
    Signed-off-by: Mamh-windows <bright.ma@mage.com>

--- src/main/java/org/jenkinsci/plugins/scriptwatcher/WatcherComputerListener.java
+++ src/main/java/org/jenkinsci/plugins/scriptwatcher/WatcherComputerListener.java
@@ -32,10 +32,7 @@ import hudson.model.Node;
 import hudson.model.TaskListener;
 import hudson.model.Computer;
 import hudson.remoting.Channel;
-import hudson.slaves.ComputerListener;
-import hudson.slaves.NodeProperty;
-import hudson.slaves.NodePropertyDescriptor;
-import hudson.slaves.OfflineCause;
+import hudson.slaves.*;
 import hudson.tasks.BatchFile;
 import hudson.tasks.Shell;
 import hudson.util.DescribableList;
@@ -45,6 +42,7 @@ import org.kohsuke.stapler.DataBoundConstructor;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -54,79 +52,129 @@ import java.util.logging.Logger;
 public class WatcherComputerListener extends ComputerListener {
     private static final Logger LOGGER = Logger.getLogger(WatcherComputerListener.class.getName());
 
+    private static final String MARKER = "#:#:#";
+    private static final String CAUSE_VAR = "CAUSE";
+    private static final String CRLF = "\r\n";
+
+    public WatcherComputerListener() {
+        LOGGER.info("构造方法: public WatcherComputerListener()");
+    }
+
     @Override
     public void preLaunch(Computer c, TaskListener taskListener) throws IOException, InterruptedException {
         LOGGER.info("public void preLaunch(Computer c, TaskListener taskListener)");
         LOGGER.info("computer: " + c);
+        LOGGER.info("taskListener: " + taskListener);
+
+        taskListener.getLogger().println("preLaunch: taskListener: " + taskListener);
+
+        if (c instanceof SlaveComputer) {
+            TaskListener slaveListener = ((SlaveComputer) c).getListener();
+            LOGGER.info("slaveListener: " + slaveListener);
+            slaveListener.getLogger().println("preLaunch: slaveListener: " + slaveListener);
+        }
     }
 
     @Override
     public void onLaunchFailure(Computer c, TaskListener taskListener) throws IOException, InterruptedException {
         LOGGER.info("public void onLaunchFailure(Computer c, TaskListener taskListener)");
         LOGGER.info("computer: " + c);
+        LOGGER.info("taskListener: " + taskListener);
+        taskListener.getLogger().println("onLaunchFailure: taskListener: " + taskListener);
+
+        if (c instanceof SlaveComputer) {
+            TaskListener slaveListener = ((SlaveComputer) c).getListener();
+            LOGGER.info("slaveListener: " + slaveListener);
+            slaveListener.getLogger().println("onLaunchFailure: slaveListener: " + slaveListener);
+        }
     }
 
     @Override
-    public void preOnline(Computer c, Channel channel, FilePath root, TaskListener listener) throws IOException, InterruptedException {
+    public void preOnline(Computer c, Channel channel, FilePath root, TaskListener taskListener) throws IOException, InterruptedException {
         LOGGER.info("public void preOnline(Computer c, Channel channel, FilePath root, TaskListener listener) ");
         LOGGER.info("computer: " + c);
+        LOGGER.info("taskListener: " + taskListener);
+        LOGGER.info("FilePath: " + root);
 
+        taskListener.getLogger().println("preOnline: taskListener: " + taskListener);
+
+        if (c instanceof SlaveComputer) {
+            TaskListener slaveListener = ((SlaveComputer) c).getListener();
+            LOGGER.info("slaveListener: " + slaveListener);
+            slaveListener.getLogger().println("preOnline: slaveListener: " + slaveListener);
+        }
     }
 
 
     @Override
-    public void onOnline(Computer c, TaskListener listener) throws IOException, InterruptedException {
+    public void onOnline(Computer c, TaskListener taskListener) throws IOException, InterruptedException {
         LOGGER.info("public void onOnline(Computer c, TaskListener listener)");
         LOGGER.info("computer: " + c);
+        LOGGER.info("taskListener: " + taskListener);
+        taskListener.getLogger().println("onOnline: taskListener: " + taskListener);
+
+        if (c instanceof SlaveComputer) {
+            TaskListener slaveListener = ((SlaveComputer) c).getListener();
+            LOGGER.info("slaveListener: " + slaveListener);
+            slaveListener.getLogger().println("onOnline: slaveListener: " + slaveListener);
+        }
     }
 
 
     @Override
     public void onOffline(@Nonnull Computer c, @CheckForNull OfflineCause cause) {
-        LOGGER.info("public void onOffline(@Nonnull Computer c, @CheckForNull OfflineCause cause)");
+        LOGGER.info("public void onOffline(Computer c, OfflineCause cause)");
         LOGGER.info("computer: " + c);
-        LOGGER.info("cause: " + cause);
-        String script = "env";
+        LOGGER.info("cause: " + cause.toString());
+
 
         final Node slave = c.getNode();
         final Node master = Jenkins.get();
 
-        if (slave == null)
+        if (slave == null) {
+            LOGGER.info("slave node is null");
             return;
-
+        }
         if (slave == master) {
             LOGGER.info("ignore master node");
             return;
         }
+        LOGGER.info("slaveListener: " + ((SlaveComputer) c).getListener());
+        TaskListener slaveListener = ((SlaveComputer) c).getListener();
+        slaveListener.getLogger().println("onOffline: slaveListener:  " + slaveListener);
 
         final DescribableList<NodeProperty<?>, NodePropertyDescriptor> properties = (slave instanceof Jenkins) ? ((Jenkins) slave).getGlobalNodeProperties() : slave.getNodeProperties();
         WatcherNodeProperty watcherNodeProperty = properties.get(WatcherNodeProperty.class);
         LOGGER.info("watcherNodeProperty = " + watcherNodeProperty);
-        if(watcherNodeProperty==null){
+        if (watcherNodeProperty == null) {
             LOGGER.info("ignore watcherNodeProperty");
             return;
         }
+        String script = watcherNodeProperty.getScripts();
         FilePath root = master.getRootPath();
+
 
         LOGGER.info("Master given path is " + root.getRemote());
 
-        final LogTaskListener listener = new LogTaskListener(LOGGER, Level.INFO);
 
         EnvVars enviroment = new EnvVars();
 
 
-        Shell shell = new Shell(script);
         FilePath scriptFile = null;
         try {
-            Launcher launcher = root.createLauncher(listener);
-            scriptFile = shell.createScriptFile(root);
-            launcher.launch().cmds(shell.buildCommandLine(scriptFile)).pwd(root).envs(enviroment).stdout(listener).join();
+            final FilePath batchFile = createScriptFile(root, script); //ws.createTextTempFile("jenkins", ".bat", makeScript(), true);
+            final String[] cmd = buildCommandLine(batchFile);
+
+            Launcher launcher = root.createLauncher(slaveListener);
+
+            launcher.launch().cmds(cmd).pwd(root).envs(enviroment).stdout(slaveListener).join();
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         } finally {
             try {
-                if (scriptFile != null)
+                if (scriptFile != null){
                     scriptFile.delete();
+                }
             } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
             }
@@ -135,11 +183,47 @@ public class WatcherComputerListener extends ComputerListener {
 
     }
 
-    private static WatcherNodeProperty getWatcherNodeProperty(final Computer computer) {
-        final Node node = computer.getNode();
-        if (node == null) return null;
-        final DescribableList<NodeProperty<?>, NodePropertyDescriptor> properties = (node instanceof Jenkins) ? ((Jenkins) node).getGlobalNodeProperties() : node.getNodeProperties();
-        return properties.get(WatcherNodeProperty.class);
+    private String[] buildCommandLine(FilePath batchFile) {
+        String[] cmd;
+        if (isUnix()) {
+            cmd = new String[]{"bash", batchFile.getRemote()};
+        } else {
+            cmd = new String[]{"cmd", "/c", "call", batchFile.getRemote()};
+        }
+        return cmd;
+    }
+
+    private FilePath createScriptFile(FilePath dir, String script) throws IOException, InterruptedException {
+        return dir.createTextTempFile("jenkins", getFileExtension(), getContents(script), false);
+    }
+
+    private boolean isUnix() {
+        return File.pathSeparatorChar == ':';
+    }
+
+    private String getFileExtension() {
+        if (isUnix()) {
+            return ".sh";
+        } else {
+            return ".bat";
+        }
+    }
+
+    private String getContents(String script) {
+        String contents = ""
+                + "@set " + CAUSE_VAR + "=" + CRLF
+                + "@echo off" + CRLF
+                + "call :TheActualScript" + CRLF
+                + "@echo off" + CRLF
+                + "echo " + MARKER + CAUSE_VAR + MARKER + "%" + CAUSE_VAR + "%" + MARKER + CRLF
+                + "goto :EOF" + CRLF
+                + ":TheActualScript" + CRLF
+                + script + CRLF;
+        if (isUnix()) {
+            return script;
+        } else {
+            return contents;
+        }
     }
 
     @Override
@@ -147,6 +231,7 @@ public class WatcherComputerListener extends ComputerListener {
         LOGGER.info("public void onTemporarilyOnline(Computer c)");
         LOGGER.info("computer: " + c);
     }
+
 
     @Override
     public void onTemporarilyOffline(Computer c, OfflineCause cause) {
