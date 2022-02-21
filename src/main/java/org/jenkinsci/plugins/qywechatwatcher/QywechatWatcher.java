54572b47923f4b1ab1dbfe04fe31f58208492802
bright.ma <bright.ma@blackshark.com>
2022-02-21 14:24:47 +0800

    add qywechat push
    Signed-off-by: bright.ma <bright.ma@blackshark.com>

--- src/main/java/org/jenkinsci/plugins/qywechatwatcher/QywechatWatcher.java
+++ src/main/java/org/jenkinsci/plugins/qywechatwatcher/QywechatWatcher.java
@@ -23,27 +23,39 @@
  */
 package org.jenkinsci.plugins.qywechatwatcher;
 
+import com.arronlong.httpclientutil.HttpClientUtil;
+import com.arronlong.httpclientutil.common.HttpConfig;
+import com.arronlong.httpclientutil.exception.HttpProcessException;
 import hudson.Plugin;
 import hudson.model.User;
 import hudson.plugins.jobConfigHistory.JobConfigHistory;
 import jenkins.model.Jenkins;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.jenkinsci.plugins.qywechatwatcher.jobConfigHistory.ConfigHistory;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
+import javax.net.ssl.SSLContext;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Logger;
-
 
 public class QywechatWatcher {
     private static final Logger LOGGER = Logger.getLogger(QywechatWatcher.class.getName());
 
-    private final @Nonnull Jenkins jenkins;
-    private final @Nonnull ConfigHistory configHistory;
+    private final @Nonnull
+    Jenkins jenkins;
+    private final @Nonnull
+    ConfigHistory configHistory;
 
     public QywechatWatcher(final @Nonnull Jenkins jenkins) {
         this.jenkins = jenkins;
@@ -78,17 +90,51 @@ public class QywechatWatcher {
 
     public String send(final QywechatWatcherNotification notification) throws MessagingException, AddressException {
         if (!notification.shouldNotify()) return null;
+        String webhookurl = notification.getRecipients();
+        String[] urls;
+        if (webhookurl.contains(",")) {
+            urls = webhookurl.split(",");
+        } else {
+            urls = new String[]{webhookurl};
+        }
+        if (urls.length == 0) return null;
 
-        final InternetAddress[] recipients = InternetAddress.parse(
-                notification.getRecipients()
-        );
-
-        if (recipients.length == 0) return null;
-
+        String data = "";
 
         LOGGER.info("will send msg: ");
-
+        for (String u : urls) {
+            try {
+                String msg = push(u, data);
+                LOGGER.info("通知结果" + msg);
+            } catch (HttpProcessException | KeyManagementException | NoSuchAlgorithmException e) {
+                LOGGER.info("通知异常" + e.getMessage());
+                e.printStackTrace();
+            }
+        }
         return "";
+    }
+
+    public static String push(String url, String data) throws HttpProcessException, KeyManagementException, NoSuchAlgorithmException {
+        HttpConfig httpConfig;
+        HttpClient httpClient;
+        HttpClientBuilder httpClientBuilder = HttpClients.custom();
+        if(url.startsWith("https")) {
+            SSLContext sslContext = SSLContexts.custom().build();
+            SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(
+                    sslContext,
+                    new String[]{"TLSv1", "TLSv1.1", "TLSv1.2"},
+                    null,
+                    NoopHostnameVerifier.INSTANCE
+            );
+            httpClientBuilder.setSSLSocketFactory(sslConnectionSocketFactory);
+        }
+
+        httpClient = httpClientBuilder.build();
+        //普通请求
+        httpConfig = HttpConfig.custom().client(httpClient).url(url).json(data).encoding("utf-8");
+
+        String result = HttpClientUtil.post(httpConfig);
+        return result;
     }
 
 }
