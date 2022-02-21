b18110c54d0f0abbfe32e4df97457f82c0ecf778
bright.ma <bright.ma@blackshark.com>
2022-02-21 15:56:35 +0800

    update license
    Signed-off-by: bright.ma <bright.ma@blackshark.com>

--- src/main/java/org/jenkinsci/plugins/qywechatwatcher/QywechatWatcherNotification.java
+++ src/main/java/org/jenkinsci/plugins/qywechatwatcher/QywechatWatcherNotification.java
@@ -1,40 +1,14 @@
-/*
- * The MIT License
- *
- * Copyright (c) 2012 Red Hat, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package org.jenkinsci.plugins.qywechatwatcher;
 
 import hudson.model.User;
 
+import javax.annotation.Nonnull;
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
-
-import javax.annotation.Nonnull;
-import javax.mail.MessagingException;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.MimeMessage;
-
 
 
 public abstract class QywechatWatcherNotification {
@@ -111,11 +85,12 @@ public abstract class QywechatWatcherNotification {
         return body.append("\n\n").append(this.getBody()).toString();
     }
 
-    protected @Nonnull Map<String, String> pairs() {
+    protected @Nonnull
+    Map<String, String> pairs() {
         final Map<String, String> pairs = new HashMap<>(3);
         pairs.put("Date", new Date().toString());
         pairs.put("Initiator", this.getInitiator().getId());
-        pairs.put("Url", "[" + this.getArtefactUrl()+ "](" + this.getArtefactUrl() + ")");
+        pairs.put("Url", "[" + this.getArtefactUrl() + "](" + this.getArtefactUrl() + ")");
         return pairs;
     }
 
